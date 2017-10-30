71b72ddf3b78f04cb9480bb4d11911efae274a41
MATTHEW JOSEPH KAVIRAYOR <matthewjosephkavirayor@MATTHEWs-MacBook-Pro.local>
2017-10-30 17:09:21 +0530

    Current code including delays and full forms of parameters

--- arduino/code.ino
+++ arduino/code.ino
@@ -16,12 +16,25 @@ void loop()
 {
   int chk = DHT.read11(DHT11_PIN);
   lcd.clear();
-  lcd.setCursor(0,0); 
-  lcd.print("T = ");//testing the commit 
-  lcd.print(DHT.temperature);
-  lcd.print("C ");//also testing commit
-  lcd.print("H =  ");
-  lcd.print(DHT.humidity);
+  lcd.setCursor(0,0); //jhkjhjbhuijhu
+  lcd.print("Reading");
+  lcd.setCursor(0,1);
+  lcd.print("Temperature");
+  delay(1000);
+  lcd.clear();
+  lcd.setCursor(0,0);\
+  lcd.print("Temp = ");
+  lcd.print(DHT.temperature);//reading temperature from sensor 
+  lcd.print("C");
+  delay(2000);
+  lcd.clear();
+  lcd.print("Reading");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity");
+  delay(1000);
+  lcd.clear();//testing git push
+  lcd.print("Humidity=");
+  lcd.print(DHT.humidity);//reading humidity from sensor
   lcd.print("%");
   delay(2000);
 }
